c0628eafa9b00697b4b09c8f269065858fcc1761
unclemohit <mohit26998@gmail.com>
2025-03-19 20:19:19 -0700

    Initial Commit

---
+++ MillColoringApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MillColoringApp
+//
+//  Created by Mohit Singh on 2025-03-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MillColoringApp/MillColoringAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillColoringAppApp.swift
+//  MillColoringApp
+//
+//  Created by Mohit Singh on 2025-03-19.
+//
+
+import SwiftUI
+
+@main
+struct MillColoringAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
